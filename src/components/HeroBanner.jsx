2d53d08a161e1a7487dfb5d8fed05b85c4ad9456
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-17 18:16:49 +0100

    progress

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -4,25 +4,29 @@ import { banner } from "../assets";
 
 const HeroBanner = () => {
   return (
-    <Box>
+    <Box sx={{ paddingLeft: { lg: "0px", xs: "20px" } }}>
       <Typography
         fontSize="26px"
         fontWeight="600"
         color="#ff2625"
-        sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+        sx={{
+          mt: { lg: "212px", xs: "70px" },
+          marginRight: { lg: "0px", sm: "50px" },
+        }}
         position="relative"
-        p="20px"
       >
         Fitness Club
       </Typography>
       <Typography
         fontWeight="700"
-        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        sx={{
+          fontSize: { lg: "44px", xs: "45px" },
+        }}
         mb="23px"
         mt="30px"
       >
         Sweat, Smile, <br />
-        and Repeat
+        and <span style={{ color: "#ff2526" }}>Repeat!</span>
       </Typography>
       <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Check out exercises that suits you best
@@ -31,7 +35,12 @@ const HeroBanner = () => {
         variant="contained"
         className="app__hero-btn"
         href="#exercises"
-        sx={{ backgroundColor: "#ff2526", padding: "10px" }}
+        sx={{
+          backgroundColor: "#ff2526",
+          padding: "8px",
+          width: { lg: "175px", xs: "200px" },
+          height: "56px",
+        }}
       >
         Explore Exercises
       </Button>
--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,12 +1,58 @@
-import React, {useEffect, useState} from 'react'
-
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography, Button, TextField } from "@mui/material";
+import { borderRadius } from "@mui/system";
 
 const SearchExercises = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight="700"
+        textTransform="capitalize"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Search for an <span style={{ color: "#ff2526" }}>excercise</span>
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search Exercises..."
+          type="text"
+          sx={{
+            input: {
+              fontWeight: "700",
+              border: "none",
+              outline: "none",
+              borderRadius: "4px",
+            },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+        />
+        <Button
+          variant="contained"
+          className="search-btn"
+          href="#exercises"
+          sx={{
+            backgroundColor: "#ff2526",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          search
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
